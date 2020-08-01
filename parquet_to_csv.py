f27e0cbb9ee5b50b3087a0175a1f72059c467271
vineet.payyappalli <vineet.payyappalli@westrock.com>
2020-08-01 19:35:24 -0400

    Modified config file, added Python script to convert parquet to csv, added output files with predictions for 5 US states for the period Jan 1, 2020 to Aug 31, 2020

---
+++ parquet_to_csv.py
@@ -0,0 +1,12 @@
+﻿### script to convert parquet files in model_output directory into CSV
+
+import os
+import pandas as pd
+
+cwd = os.getcwd()
+model_output_path = os.path.join(cwd, 'model_output', 'state5_None')
+
+for filename in os.listdir(model_output_path):
+	if filename.endswith('.parquet'):
+		df = pd.read_parquet(os.path.join(model_output_path, filename))
+		df.to_csv(os.path.join(model_output_path, filename.replace('parquet', 'csv')))
